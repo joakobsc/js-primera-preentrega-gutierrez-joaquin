335903d4a73a81d37ee5840063e481c3a0bfcfc1
Joaquin <joakobsc@gmail.com>
2024-04-29 22:36:25 -0300

    subiendo primera entrega de js

---
+++ main.js
@@ -0,0 +1,33 @@
+let telefono
+
+alert("Estamos buscando desarrolladores que nos ayuden en nuestro trabajo")
+
+function númeroDeT(){
+    telefono = parseInt(prompt("ingresá tu número de telefono"))
+    telefono = confirm ( "El número "+telefono+ " " + "es correcto?")
+    if(telefono == false){
+        telefono = parseInt(prompt("ingresá el número de telefono nuevamente"))
+    }
+}
+
+let buscarTrabajo = confirm("¿Deseas trabajar con nosotros?")
+
+while(buscarTrabajo){
+    let puesto= prompt("¿En qué área desea trabajar? Digite la letra correspondiente A. Desarrollador-Frontend B. Desarrollador backend");
+
+    if(puesto == ""){
+        alert("por favor ingresá una opción")
+    }else if (puesto == "A" || puesto == "B"){
+        númeroDeT()
+        alert("Te hablaremos a la brevedad ¡Gracias por contactarnos!")
+        buscarTrabajo = false
+    }
+    else{
+        alert ("la opción ingresada no es valida")
+        buscarTrabajo = confirm ("¿Deseas trabajar con nosotros?")
+    }
+
+    
+}
+
+alert ("Gracias por tenernos en cuenta")
